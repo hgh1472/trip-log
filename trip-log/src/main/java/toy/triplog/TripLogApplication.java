14d3f7e61d58d6909d62be20b76cee387b75b119
hgh1472 <hgh1472@naver.com>
2025-03-25 14:38:20 +0900

    chore: init project

---
+++ trip-log/src/main/java/toy/triplog/TripLogApplication.java
@@ -0,0 +1,13 @@
+package toy.triplog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripLogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TripLogApplication.class, args);
+    }
+
+}
