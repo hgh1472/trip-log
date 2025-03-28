4fe7b957a57f7fcbca4b212dc83c90029ff2327f
hgh1472 <hgh1472@naver.com>
2025-03-28 15:14:50 +0900

    test : 유저 패스워드 암호화 테스트 추가

---
+++ trip-log/src/test/java/toy/triplog/security/PasswordManagerTest.java
@@ -0,0 +1,31 @@
+package toy.triplog.security;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import toy.triplog.domain.user.UserSignInfo;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+class PasswordManagerTest {
+
+    @Autowired
+    private PasswordManager passwordManager;
+
+    @DisplayName("유저의 패스워드를 암호화한 로그인 정보를 반환한다.")
+    @Test
+    void encodePassword() {
+        // given
+        UserSignInfo userSignInfo = new UserSignInfo("email@email.com", "password");
+
+        // when
+        UserSignInfo encodedUserSignInfo = passwordManager.encodePassword(userSignInfo);
+
+        // then
+        assertThat(encodedUserSignInfo.getEmail()).isEqualTo("email@email.com");
+        assertThat(encodedUserSignInfo.getPassword()).isNotEqualTo("password");
+    }
+
+}
