a66349284885c0f01e03ad5a2d27f9d726c2677e
hgh1472 <hgh1472@naver.com>
2025-04-29 21:42:50 +0900

    refactor: 불필요한 어노테이션 제거

--- trip-log/trip-api/src/main/java/toy/triplog/TripLogApplication.java
+++ trip-log/trip-api/src/main/java/toy/triplog/TripLogApplication.java
@@ -2,9 +2,7 @@ package toy.triplog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
-@ConfigurationPropertiesScan
 @SpringBootApplication
 public class TripLogApplication {
 
