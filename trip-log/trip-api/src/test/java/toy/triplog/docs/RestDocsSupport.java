0c508d99633ad9adaa49b2ec105baef0d4159fa0
hgh1472 <hgh1472@naver.com>
2025-05-01 15:19:10 +0900

    feat: REST Docs 통합 환경 설정 추가

---
+++ trip-log/trip-api/src/test/java/toy/triplog/docs/RestDocsSupport.java
@@ -0,0 +1,31 @@
+package toy.triplog.docs;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.restdocs.RestDocumentationContextProvider;
+import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+
+@ExtendWith(RestDocumentationExtension.class)
+@SpringBootTest
+public abstract class RestDocsSupport {
+
+    protected MockMvc mockMvc;
+    protected ObjectMapper objectMapper = new ObjectMapper();
+
+    @BeforeEach
+    void setUp(WebApplicationContext context, RestDocumentationContextProvider provider) {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(context)
+                .apply(documentationConfiguration(provider))
+                .apply(springSecurity())
+                .build();
+    }
+
+}
